6e859fcce352d15d337f617dc3ba37334182a286
秦政 <345020459@qq.com>
2018-11-26 18:44:11 +0800

    提交Dv1

---
+++ Git_test/src/main/java/cn/itcast/testDv1.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+public class testDv1 {
+    public void testDv1(){
+        System.out.println("分支");
+    }
+    public void testDv2(){
+        System.out.println("这个是分支master看不到");
+    }
+
+}
